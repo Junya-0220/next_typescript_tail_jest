c0ef280386db88794fcdcbad934d31238ba58df1
Junya <junya010220@gmail.com>
2021-02-12 23:14:58 +0900

    Blogページのテストを書いた

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
 
+//function 式で書かずに arrow function で書きなおしたらエラーは消えた。
+// function MyApp({ Component, pageProps }): AppProps {
+//   return <Component {...pageProps} />
+// }
 
-function MyApp({ Component, pageProps }): AppProps {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
-}
+};
 
 export default MyApp
